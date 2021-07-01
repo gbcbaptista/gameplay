a9a657c86a77f33107332742b1968f2b59c7227d
Gabriel <gbcbaptista@gmail.com>
2021-07-01 20:15:10 -0300

    Usando endpoint para obter servidores do usuario

--- src/components/Load/index.tsx
+++ src/components/Load/index.tsx
@@ -3,11 +3,15 @@ import { View, ActivityIndicator } from 'react-native';
 import { theme } from "../../global/styles/theme";
 import { styles } from './styles';
 
-export function Load(){
+const Load = () => {
+
+  return (
   <View style={styles.container}>
     <ActivityIndicator
       size = "large"
       color={theme.colors.primary}
     />
   </View>
-}
+  )}
+
+export default Load;
--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -11,6 +11,10 @@ import { Background } from '../../components/Background';
 import { Guild, GuildProps } from '../../components/Guild';
 import { ListDivider } from '../../components/ListDivider';
 import { useState } from 'react';
+import Load from '../../components/Load';
+import { api } from '../../services/api';
+import { useEffect } from 'react';
+
 
 type Props = {
   handleGuildSelect: (guild: GuildProps) => void;
@@ -21,24 +25,37 @@ export function Guilds({ handleGuildSelect }: Props){
   const [guilds, setGuilds] = useState<GuildProps[]>([])
   const [loading, setLoading] = useState(true)
 
+  const fetchGuilds = async () => {
+    const response = await api.get('use/@me/guilds')
+
+    setGuilds(response.data);
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchGuilds();
+  }, [])
+
   return (
 
       <View style={styles.container}>
-        <FlatList 
-          data={guilds}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <Guild 
-              data={item}
-              onPress={() => handleGuildSelect(item)}
-            />
-          )}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{paddingBottom: 68, paddingTop: 103 }}
-          ListHeaderComponent={() => <ListDivider widthPercent={'70%'} isCentered/>}
-          ItemSeparatorComponent={() => <ListDivider widthPercent={'70%'} isCentered/>}
-          style={styles.guilds}
-        />
+        { loading ? <Load /> :
+         <FlatList 
+            data={guilds}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Guild 
+                data={item}
+                onPress={() => handleGuildSelect(item)}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom: 68, paddingTop: 103 }}
+            ListHeaderComponent={() => <ListDivider widthPercent={'70%'} isCentered/>}
+            ItemSeparatorComponent={() => <ListDivider widthPercent={'70%'} isCentered/>}
+            style={styles.guilds}
+          />
+        }
       </View>
 
   );
